0341547c9c5d53e75911d31d617cfd72f4e483ae
ravisinghrajput95 <ravisinghrajput005@gmail.com>
2023-01-07 12:56:25 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mttdemo_devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# mttdemo-devops
